a89505856cd10848901aa26f4c9f625581b8542e
fll-pigeons <67392993+fll-pigeons@users.noreply.github.com>
2020-07-09 15:26:39 -0400

    Create dimensions.py

---
+++ programs/dimensions.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env pybricks-micropython
+
+"""
+Example LEGO® MINDSTORMS® EV3 Robot Educator Driving Base Program
+-----------------------------------------------------------------
+
+This program requires LEGO® EV3 MicroPython v2.0.
+Download: https://education.lego.com/en-us/support/mindstorms-ev3/python-for-ev3
+
+Building instructions can be found at:
+https://education.lego.com/en-us/support/mindstorms-ev3/building-instructions#robot
+"""
+
+from pybricks.hubs import EV3Brick
+from pybricks.ev3devices import Motor
+from pybricks.parameters import Port
+from pybricks.robotics import DriveBase
+
+# Initialize the EV3 Brick.
+ev3 = EV3Brick()
+
+# Initialize the motors.
+left_motor = Motor(Port.B)
+right_motor = Motor(Port.C)
+lift_motor = Motor(Port.A)
+
+# Initialize the drive base.
+robot = DriveBase(left_motor, right_motor, wheel_diameter=54.6, axle_track=104.1)
+
+
+
+
+
+
+
+
+
+
+
+ev3.screen.draw_text(50, 60, "Pigeons!")
+ev3.speaker.beep()
+robot.turn(360)
+ev3.speaker.beep()
