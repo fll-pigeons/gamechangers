37452e65830a836dec44393ad314c5ce29c95d37
fll-pigeons <67392993+fll-pigeons@users.noreply.github.com>
2020-07-10 17:34:17 -0400

    Create recalibrate_tests.py

---
+++ programs/recalibrate_tests.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env pybricks-micropython
+
+"""
+Example LEGO® MINDSTORMS® EV3 Robot Educator Driving Base Program
+-----------------------------------------------------------------
+
+This program requires LEGO® EV3 MicroPython v2.0.
+Download: https://education.lego.com/en-us/support/mindstorms-ev3/python-for-ev3
+
+Building instructions can be found at:
+https://education.lego.com/en-us/support/mindstorms-ev3/building-instructions#robot
+"""
+
+from pybricks.hubs import EV3Brick
+from pybricks.ev3devices import Motor
+from pybricks.ev3devices import GyroSensor
+from pybricks.parameters import Direction
+from pybricks.parameters import Port
+from pybricks.robotics import DriveBase
+from pybricks.tools import wait
+
+# Initialize the EV3 Brick.
+ev3 = EV3Brick()
+
+# Initialize the motors.
+left_motor = Motor(Port.B)
+right_motor = Motor(Port.C)
+
+
+# Initialize the drive base.
+robot = DriveBase(left_motor, right_motor, wheel_diameter=54.6, axle_track=104.1)
+gyro = GyroSensor(Port.S1)
+
+
+#################################################################################
+
+ev3.screen.draw_text(50, 60, "Pigeons!")
+ev3.speaker.beep()
+# recalibrate gyro
+gyro.speed()
+wait(2)
+gyro.angle()
+ev3.speaker.beep()
+ev3.speaker.beep()
+
+while robot.distance() < 500:
+    robot.drive(1000, 0)
+
+robot.reset()
+gyro.reset_angle(0)
+angle = gyro.angle()
+print (  "gyro1 " + str(angle) )
+while abs(angle) < 160:
+    print (  "gyro2 " + str(angle) )
+    print (  "drive " + str(robot.angle() ) )    
+    robot.drive(10, -180)
+    angle = gyro.angle()
+
+robot.reset()
+while robot.distance() < 500:
+    robot.drive(1000, 0)
