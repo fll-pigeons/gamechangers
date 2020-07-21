9894c362287b606593c23bdbdaa51cfc42772e34
Pigeons <67392993+fll-pigeons@users.noreply.github.com>
2020-07-21 10:38:08 -0400

    Update and rename LP03_squareLoop2.py to LP03_squareLoopDistanceTurn.py

--- programs/LP03_squareLoopDistanceTurn.py
+++ programs/LP03_squareLoopDistanceTurn.py
@@ -10,7 +10,7 @@ https://education.lego.com/en-us/support/mindstorms-ev3/building-instructions#ro
 """
 
 from pybricks.hubs import EV3Brick
-from pybricks.ev3devices import Motor
+from pybricks.ev3devices import Motor, GyroSensor
 from pybricks.parameters import Port
 from pybricks.robotics import DriveBase
 from pybricks.tools import wait
@@ -21,30 +21,32 @@ ev3 = EV3Brick()
 # Initialize the motors.
 left_motor = Motor(Port.B)
 right_motor = Motor(Port.C)
+left_motor.reset_angle(0)
+right_motor.reset_angle(0)
 
+# Initialize gyro.
+gyro = GyroSensor(Port.S3)
+gyro.reset_angle(0)
 
 # Initialize the drive base.
 robot = DriveBase(left_motor, right_motor, wheel_diameter=54.6, axle_track=104.1)
-
-
 
 #################################################################################
 
 ev3.screen.draw_text(50, 60, "Pigeons!")
 
-
-
 x=0
-while x < 8:
+while x < 16:
     # straight (if run motor faster, must adjust distance)
-    robot.reset()
+    robot.reset()   
     while robot.distance() < 250:
         robot.drive(200 ,0)
 
+    print ("gyro")
     # turn (if run motor faster, must adjust angle)
-    robot.reset()
-    while robot.angle() < 75:
-        print ("angle: " + str(robot.angle()))
+    gyro.reset_angle(0)
+    while abs(gyro.angle()) < 66:
+        print ("angle: " + str(abs(gyro.angle())))
         robot.drive(200 ,150)
     x = x + 1
 robot.stop()
