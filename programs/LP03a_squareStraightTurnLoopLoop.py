17d9d0039028c1d60062fbed1816da31f0c3beb2
Pigeons <67392993+fll-pigeons@users.noreply.github.com>
2020-07-21 11:01:36 -0400

    Create LP03a_squareStraightTurnLoopLoop.py

---
+++ programs/LP03a_squareStraightTurnLoopLoop.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env pybricks-micropython
+
+"""
+Example LEGO® MINDSTORMS® EV3 Robot Educator Driving Base Program
+-----------------------------------------------------------------
+This program requires LEGO® EV3 MicroPython v2.0.
+Download: https://education.lego.com/en-us/support/mindstorms-ev3/python-for-ev3
+Building instructions can be found at:
+https://education.lego.com/en-us/support/mindstorms-ev3/building-instructions#robot
+"""
+
+from pybricks.hubs import EV3Brick
+from pybricks.ev3devices import Motor
+from pybricks.parameters import Port
+from pybricks.robotics import DriveBase
+from pybricks.tools import wait
+
+# Initialize the EV3 Brick.
+ev3 = EV3Brick()
+
+# Initialize the motors.
+left_motor = Motor(Port.B)
+right_motor = Motor(Port.C)
+
+
+# Initialize the drive base.
+robot = DriveBase(left_motor, right_motor, wheel_diameter=54.6, axle_track=104.1)
+
+
+
+#################################################################################
+
+ev3.screen.draw_text(50, 60, "Pigeons!")
+
+
+robot.settings(300 ,250 ,150 ,100)
+
+x=0
+while x < 3:
+    while x < 4:
+        robot.straight(340)
+        robot.turn(88)
