9f60179cf156aa397b6ef308c8535128452bf379
Pigeons <67392993+fll-pigeons@users.noreply.github.com>
2020-08-11 16:16:41 -0400

    Create M01_M14_M06_M07.py
    first try

---
+++ programs/M01_M14_M06_M07.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env pybricks-micropython
+
+"""
+Example LEGO® MINDSTORMS® EV3 Robot Educator Driving Base Program
+-----------------------------------------------------------------
+
+This program requires LEGO® EV3 MicroPython v2.0.
+Download: https://education.lego.com/en-us/support/mindstorms-ev3/python-for-ev3
+
+Building instructions can be found at:
+https://education.lego.com/en-us/support/mindstorms-ev3/building-instructions#robot
+"""
+
+from pybricks.hubs import EV3Brick
+from pybricks.ev3devices import Motor
+from pybricks.parameters import Port
+from pybricks.robotics import DriveBase
+
+# Initialize the EV3 Brick.
+ev3 = EV3Brick()
+
+# Initialize the motors.
+left_motor = Motor(Port.B)
+right_motor = Motor(Port.C)
+lift_motor = Motor(Port.A)
+
+# Initialize the drive base.
+robot = DriveBase(left_motor, right_motor, wheel_diameter=94.2, axle_track=60)
+
+robot.settings (620, 620, 620, 620)
+ev3.screen.draw_text(50, 60, "Pigeons!")
+ev3.speaker.beep()
+
+
+robot.straight(669)
+robot.straight(-270)
+robot.turn (70)
+robot.straight(250)
+robot.turn (-70)
+robot.straight(400)
+robot.turn(-170)
+robot.straight(550)
+robot.turn(-100)
+robot.straight(300)
+robot.straight(50)
+robot.straight(-50)
+robot.straight(50)
+robot.straight(-50)
+robot.straight(50)
+robot.straight(-50)
+robot.straight(50)
+robot.straight(-50)
+robot.straight(50)
+robot.straight(-50)
+robot.straight(50)
+robot.straight(-50)
