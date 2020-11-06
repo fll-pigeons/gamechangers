3bb6ef00beff9f9e0b1708cee34658066a8e6ea8
Pigeons <67392993+fll-pigeons@users.noreply.github.com>
2020-11-05 20:05:01 -0500

    Update main.py

--- programs/main.py
+++ programs/main.py
@@ -3,7 +3,7 @@
 
 from pybricks.hubs import EV3Brick
 from pybricks.ev3devices import Motor
-from pybricks.parameters import Port
+from pybricks.parameters import Port, Direction
 from pybricks.robotics import DriveBase
 
 # Initialize the EV3 Brick.
@@ -12,15 +12,15 @@ ev3 = EV3Brick()
 # Initialize the motors.
 left_motor = Motor(Port.B)
 right_motor = Motor(Port.C)
-forklift_motor = Motor(Port.D)
+forklift_motor = Motor(Port.D) 
 
 # Initialize the drive base.
 robot = DriveBase(left_motor, right_motor, wheel_diameter=94.2, axle_track=94)
 
-ev3.screen.draw_text(50, 60, "NUKE INCOMING")
+ev3.screen.draw_text(50, 60, "Testing!")
 
 ev3.speaker.beep()
-#forklift_motor.run_angle(speed=99, rotation_angle=650)
-#forklift_motor.run_angle(speed=99, rotation_angle=-650)
+forklift_motor.run_angle(speed=99, rotation_angle=360)
+forklift_motor.run_angle(speed=99, rotation_angle=-360)
 
 ev3.speaker.beep()
