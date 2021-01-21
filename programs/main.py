889f136374437bd16ac37535a354d6403bb0987a
Pigeons <67392993+fll-pigeons@users.noreply.github.com>
2021-01-21 15:40:44 -0500

    Update main.py

--- programs/main.py
+++ programs/main.py
@@ -12,19 +12,20 @@ ev3 = EV3Brick()
 # Initialize the motors.
 left_motor = Motor(Port.B)
 right_motor = Motor(Port.C)
-forklift_motor = Motor(Port.D) 
+forklift_motor = Motor(Port.D,  positive_direction=Direction.COUNTERCLOCKWISE) 
 
 # Initialize the drive base.
 robot = DriveBase(left_motor, right_motor, wheel_diameter=94.2, axle_track=94)
 
-ev3.screen.draw_text(50, 60, "DR.T's MEGA CRAB")
+ev3.screen.draw_text(50, 60,"Testing!")
 
 ev3.speaker.beep()
-#forklift_motor.run_angle(speed=25198, rotation_angle=10000)
-#forklift_motor.run_angle(speed=25198, rotation_angle=-10000)
+#forklift_motor.run_angle(speed=5005, rotation_angle=2*360)
+forklift_motor.run_angle(speed=5005, rotation_angle=-2*360)
 
 
+
 #forklift_motor.run_angle(speed=25198, rotation_angle=10000)
-#forklift_motor.run_angle(speed=25198, rotation_angle=-10000)
+#forklift_motor.run_angle(speed=25198, rotation_angle=-8500)
 
 ev3.speaker.beep()
