d2167157f702627fee46eb67eece98db264e2872
Pigeons <67392993+fll-pigeons@users.noreply.github.com>
2021-02-11 20:28:04 -0500

    Create 2_benchlift.py

---
+++ programs/2_benchlift.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env pybricks-micropython
+
+from pybricks.hubs import EV3Brick
+from pybricks.ev3devices import Motor, GyroSensor
+from pybricks.parameters import Port, Direction
+from pybricks.robotics import DriveBase
+from pybricks.tools import wait
+
+# Initialize the EV3 Brick.
+ev3 = EV3Brick()
+
+# Initialize the motors.
+left_motor = Motor(Port.B,  positive_direction=Direction.COUNTERCLOCKWISE)
+right_motor = Motor(Port.C,  positive_direction=Direction.COUNTERCLOCKWISE)
+boccia_motor = Motor(Port.A)
+forklift_motor = Motor(Port.D) 
+
+robot = DriveBase(left_motor, right_motor, wheel_diameter=94.2, axle_track=94)
+robot.settings(straight_speed=200 , straight_acceleration=50, turn_rate=150, turn_acceleration=200)
+ev3.screen.draw_text(50, 60, "Drop the blocks!")
+ev3.speaker.beep()
+
+gyro_sensor = GyroSensor(Port.S2, Direction.COUNTERCLOCKWISE)
+
+def gyro_turn(angle, speed):
+    gyro_sensor.reset_angle(0)
+    if angle < 0:
+        while gyro_sensor.angle() > angle:
+            left_motor.run(speed=(-1 * speed))
+            right_motor.run(speed=speed)
+            wait(10)
+    elif angle > 0:  
+        while gyro_sensor.angle() < angle:
+            left_motor.run(speed=speed)
+            right_motor.run(speed=(-1 * speed))
+            wait(10)  
+    else:
+        print("Error: no angle chosen")
+
+    left_motor.brake()
+    right_motor.brake() 
+
+def gyro_straight(distance, robotSpeed):
+    robot.reset() 
+    gyro_sensor.reset_angle(0)
+
+    PROPORTIONAL_GAIN = 1.2
+    if distance < 0: # move backwards
+        while robot.distance() > distance:
+            reverseSpeed = -1 * robotSpeed        
+            angle_correction = -1 * PROPORTIONAL_GAIN * gyro_sensor.angle()
+            robot.drive(reverseSpeed, angle_correction) 
+            wait(10)
+    elif distance > 0: # move forwards             
+        while robot.distance() < distance:
+            angle_correction = -1 * PROPORTIONAL_GAIN * gyro_sensor.angle()
+            robot.drive(robotSpeed, angle_correction) 
+            wait(10)            
+    robot.stop()
+
+#go to bench
+gyro_straight(350, 100)
+gyro_turn(10, 100)
+#take off backrest
+forklift_motor.run_angle(speed=400, rotation_angle=-360)
+forklift_motor.run_angle(speed=400, rotation_angle=360)
+#robot goes back home
+gyro_straight(-430, 100)
